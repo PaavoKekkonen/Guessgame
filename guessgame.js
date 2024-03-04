408f3837180661ae47e6bdc1219bd7ebf9ae9e15
PaavoKekkonen <kekkonenpaavo96@gmail.com>
2024-03-04 15:01:12 +0200

    add functionality

--- guessgame.js
+++ guessgame.js
@@ -1,94 +1,79 @@
 ////////////////// A R V A U S P E L I ///////////////////
 
-/*
- * Arvauspelissä tehtäväsi on arpoa luku esimerkiksi yhden
- * ja kymmenen väliltä ja sitten antaa käyttäjän arvata
- * numeroa kunnes hän arvaa oikein. Pelin voi myös aloittaa
- * alusta.
- *
- * Tehtävän maksimimistemäärä on 10p
- */
+let number;
 
-/*
- * Mikäli käytät VSCodea, voit asentaa Live Server -laajennuksen,
- * joka luo sinulle palvelimen ja päivittää sivun aina kun tallennat
- * tiedoston. Tämä helpottaa kehitystyötä, koska sinun ei tarvitse
- * käynnistää palvelinta erikseen.
- *
- * Live Serverin asennusohjeet:
- * https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
- *
- */
-
-/*
- * Tee tähän muuttuja, johon arvattava numero tallennetaan.
- * Muuttuja on funktioiden ulkopuolella, jotta se näkyy myös
- * muihin funktioihin.
- */
-
-// val , var , const .. mites ne muuttujat nyt menikään? ja tähän varmaan nyt joku null tai jotain ennen kuin päästään arpomaan
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
-
-/* Start-funktiota kutsutaan heti kun sivu ladataan, mutta
- * Start-funktio määritellään myöhemmin.
- */
 start();
 
-/*
- * Kirjoita tähän koodi, joka arpoo satunnaisnumeron,
- * ja tallentaa sen yo. muuttujaan.
- * Math.random() on hyvä tapa tuottaa satunnaislukuja,
- * ja esimerkiksi Math.floor() on helppo tapa muodostaa
- * desimaaliluvusta kokonaisluku jättämällä desimaalit
- * pois tai Math-round(), joka pyöristää lähimpään kokonaislukuun.
- */
 function start() {
   console.log("Start");
+  number = Math.floor(Math.random() * 10 + 1)
+  feedback.innerHTML = "";
+  document.querySelector("#guess").value = '';
 }
 
-/*
- * Lisätään guessButton-napille kuuntelija, joka kutsuu
- * numeronarvausfunktiota.
- *
- * Tämä tarkoittaa sitä, että joka kerta kun käyttäjä painaa nappia,
- * funktio guessNumber() suoritetaan.
- */
-
 const guessButton = document.querySelector("#guessButton");
+
 guessButton.addEventListener("click", function (event) {
   guessNumber();
 });
 
-/*
- * Kirjoita tähän koodi, joka ottaa arvon
- * tekstikentästä, tarkistaa onko se isompi, pienempi
- * vai oikea numero, ja antaa käyttäjälle palautteen
- * feedback-diviin. Voit lisätä palautteelle luokan
- * too-small tai too-big näyttääksesi palautteen myös
- * värinä. Tyhjennä samalla tekstikenttä uutta arvausta
- * varten.
- * Käytettäviä eri elementtejä voi hakea
- * document.getElementById()-funktiolla. Feedback-
- * elementin sisälle voi asettaa html-sisältöä innerHTML-
- * muuttujalla.
- */
+const guessInput = document.querySelector("#guess");
+
+guessInput.addEventListener("click", function (event) {
+  document.querySelector("#guess").value = '';
+})
+
+guessInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    guessButton.click();
+  }
+})
+
+
 function guessNumber() {
   console.log("Guess made");
+  const guess = Number(guessInput.value)
+  const feedback = document.querySelector("#feedback")
 
-  /*
-   * Suosittelemme käyttämään switch/case rakennetta
-   * tässä tehtävässä, mutta voit käyttää myös if-else
-   * ketjua
-   *
-   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
-   */
+  console.log(guess)
+  console.log(number)
+
+  if (guess > 0 && guess <= 10) {
+    switch (true) {
+      case guess < number:
+        feedback.innerHTML = "Isompi";
+        feedback.className = 'too-small';
+        break;
+      case guess > number:
+        feedback.innerHTML = "Pienempi";
+        feedback.className = 'too-big';
+        break;
+      case guess === number:
+        feedback.innerHTML = "Arvasit oikein!";
+        feedback.className = 'correct';
+        guessButton.disabled = true;
+        guessInput.disabled = true;
+        break;
+      default:
+        feedback.innerHTML = "Jotain meni pieleen.";
+        break;
+    }
+  }
+  else {
+    document.querySelector("#feedback").innerHTML = "Anna numero väliltä 1-10";
+  }
 }
 
-/*
- * Lisää samanlainen kuuntelija kuin yllä myös reset-napille
- * (id resetButton) ja kutsu siinä reset()-funktiota.
- */
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", function (event) {
+  reset();
+});
 
 function reset() {
   console.log("Reset");
+
+  guessButton.disabled = false;
+  guessInput.disabled = false;
+
+  start();
 }
